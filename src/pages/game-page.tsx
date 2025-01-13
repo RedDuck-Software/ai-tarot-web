f0c2edacb37cfed763777af837ca9748491de239
Pasha Sergiychuk <pashasergiychuk30@gmail.com>
2025-01-13 17:13:50 +0100

    fix: tarot line & button scroll

--- src/pages/game-page.tsx
+++ src/pages/game-page.tsx
@@ -1,17 +1,20 @@
+import { useLayoutEffect } from 'react';
+
 import { AboutSection } from '@/components/common/About';
 import { Header } from '@/components/common/Header';
 import { TarotLine } from '@/components/common/TarotLine';
 import { GameSection } from '@/components/pages/game/game';
+import { WORDS } from '@/constants/words';
 
 export default function GamePage() {
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
   return (
     <>
       <Header />
       <GameSection />
-      <TarotLine
-        className="border-t border-customBlack"
-        words={new Array(10).fill(['Love', 'Money', 'Life', 'Deals', 'Work']).flat(Infinity)}
-      />
+      <TarotLine className="border-t border-customBlack" words={new Array(1).fill(WORDS).flat(Infinity)} />
       <AboutSection />
     </>
   );
