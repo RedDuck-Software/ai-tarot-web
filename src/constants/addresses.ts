3907960a66bec2a59e9ca1cdf867ffe851ecc5cb
NikiTaysRD <nikitays101@gmail.com>
2025-01-14 17:45:12 +0200

    feat: addresses

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,6 +4,6 @@ import { PublicKey } from '@solana/web3.js';
 export const wSolMint = new PublicKey('So11111111111111111111111111111111111111112');
 
 export const OwnerAddress = {
-  [WalletAdapterNetwork.Mainnet]: new PublicKey('9Her7ga9XNUdjGj8utwkze9v2d1k6aZ9tac9SzJweED8'),
+  [WalletAdapterNetwork.Mainnet]: new PublicKey('2Mko2nLhSiehXGJDseYCj6hYQdrK3cKNMetcGaXtbrJk'),
   [WalletAdapterNetwork.Devnet]: new PublicKey('9Her7ga9XNUdjGj8utwkze9v2d1k6aZ9tac9SzJweED8'),
 };
