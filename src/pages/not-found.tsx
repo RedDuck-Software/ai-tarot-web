0b5694f9c77be938c29b1806ad85bb8f30a59b44
Yarre <yaroslav.syvukha@gmail.com>
2025-01-15 17:22:25 +0100

    feat: improve 404

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -7,6 +7,7 @@ export default function NotFoundPage() {
   return (
     <div className="relative flex h-screen flex-col items-center gap-6 pb-[52px] pt-20 md:min-h-[865px] md:pb-[72px] md:pt-20">
       <h1 className="text-center font-bona-nova-sc text-[80px]">404</h1>
+      <p className="px-8 text-center text-[20px]">The page you are looking for does not exist.</p>
       <Link to={routes.HOME}>
         <Button variant={'outline'} className="min-h-[60px] w-full text-[22px] font-light">
           Go Home
