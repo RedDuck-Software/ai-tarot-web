22582a5f90a89fdf444c872a704b4106b72200ad
Yarre <yaroslav.syvukha@gmail.com>
2025-01-20 14:45:51 +0100

    chore: fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 import { ToastContainer } from 'react-toastify';
 
-import { FullPageLoader } from '@/components/common/loader';
-import { QueryProvider } from '@/providers/query-provider';
-import { RouterProvider } from '@/providers/router-provider';
-import { SolanaProvider } from '@/providers/solana-provider';
+import { FullPageLoader } from './components/common/loader';
+import { QueryProvider } from './providers/query-provider';
+import { RouterProvider } from './providers/router-provider';
+import { SolanaProvider } from './providers/solana-provider';
 
 import '@fontsource/bona-nova-sc/400.css';
 import '@fontsource/bona-nova-sc/700.css';
